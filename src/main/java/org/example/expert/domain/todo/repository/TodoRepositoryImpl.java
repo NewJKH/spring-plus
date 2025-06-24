269804449c0f45dd40f6f94df8dc366645bf2661
NewJKH <newsun0805@gmail.com>
2025-06-24 18:48:30 +0900

    feat: "Lv2-3) QueryDSL 을 사용한 구현체 추가"

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
@@ -0,0 +1,28 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.QTodo;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.QUser;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoRepositoryImpl implements TodoRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId){
+        QTodo todo = QTodo.todo;
+        QUser user = QUser.user;
+
+        return Optional.ofNullable(jpaQueryFactory
+                .select(todo)
+                .from(todo)
+                .join(todo.user, user).fetchJoin()
+                .where(todo.id.eq(todoId)).fetchOne());
+    }
+}
