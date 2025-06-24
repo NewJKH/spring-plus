ec75bd52643d1a2d2cae5c37d08db4c3d65263c6
NewJKH <newsun0805@gmail.com>
2025-06-24 18:48:06 +0900

    ref: "Lv2-3) QueryDSL 위해 Custom Repository 로 분리"

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,10 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
     @Query("SELECT t " +
             "FROM Todo t " +
@@ -31,9 +29,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
                                                   @Param("startAt")  LocalDateTime startAt,
                                                   @Param("endAt") LocalDateTime endAt,
                                                   Pageable pageable);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryCustom.java
@@ -0,0 +1,11 @@
+package org.example.expert.domain.todo.repository;
+
+import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TodoRepositoryCustom {
+    Optional<Todo> findByIdWithUser(Long todoId);
+}
