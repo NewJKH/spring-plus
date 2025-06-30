5b32a5c180b66ea6ea290d7dc00479a7823cfaa9
NewJKH <newsun0805@gmail.com>
2025-06-30 16:40:01 +0900

    feat : "로그 저장 추가"

---
+++ src/main/java/org/example/expert/domain/log/LogRepository.java
@@ -0,0 +1,6 @@
+package org.example.expert.domain.log;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LogRepository extends JpaRepository<org.example.expert.domain.log.Log, Long> {
+}
