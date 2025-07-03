532021727cd95ff2c425f76d8740132685c7d0cd
NewJKH <newsun0805@gmail.com>
2025-07-03 19:28:57 +0900

    test : "Lv13 대용량 처리 ) 테스트용 유저 100만건 생성 "

---
+++ src/test/java/org/example/expert/domain/user/repository/UserRepositoryTest.java
@@ -0,0 +1,58 @@
+package org.example.expert.domain.user.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceContext;
+import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.example.expert.domain.user.service.UserService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // BeforeAll에서 Autowired 사용 가능
+public class UserRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    UserRepository userRepository;
+
+    private final int TOTAL_USERS = 1_000_000;
+    private final int BATCH_SIZE = 1000;
+
+    @Test
+    @Order(1)
+    @Transactional
+    @Commit
+    void insertUsers() {
+        for (long i = 0; i < TOTAL_USERS; i++) {
+            String email = "test1234_" + i + "@test.com";
+            String nickname = "user_" + i;
+
+            User user = new User(i, email, nickname, email, UserRole.USER);
+            em.persist(user);
+
+            if (i % BATCH_SIZE == 0) {
+                em.flush();
+                em.clear();
+            }
+        }
+
+        em.flush();
+        em.clear();
+    }
+}
