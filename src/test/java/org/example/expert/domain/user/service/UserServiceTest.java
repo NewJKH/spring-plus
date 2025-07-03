15b31c72a55365634b5e9eaed68ed5b2a9fbd484
NewJKH <newsun0805@gmail.com>
2025-07-03 10:54:32 +0900

    test : "Lv13 대용량 처리 ) 테스트용 1건 조회 성공 "

---
+++ src/test/java/org/example/expert/domain/user/service/UserServiceTest.java
@@ -0,0 +1,43 @@
+package org.example.expert.domain.user.service;
+
+import org.example.expert.domain.user.dto.response.UserResponse;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.awaitility.Awaitility.given;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    @DisplayName("유저이름_1건_조회_성공")
+    void getUsersByNickName() {
+        //g
+        User user = mock(User.class);
+        ReflectionTestUtils.setField(user,"id",1L);
+        ReflectionTestUtils.setField(user,"nickname","장군호");
+
+        when(userRepository.getUserByNickname("장군호")).thenReturn(user);
+        //w
+        UserResponse userResponse = userService.getUserByNickName("장군호");
+        //t
+        assertEquals(user.getId(), userResponse.getId());
+
+    }
+}
